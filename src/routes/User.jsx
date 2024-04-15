3f61502281aaf9e843d8e93944a1bddf353c7d25
sebascioffi <sebacioffi12@gmail.com>
2024-04-15 17:35:30 -0300

    tokenerr4

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -131,8 +131,7 @@ const User = () => {
             }
     }
 
-    const removeLink = async (event, linkId) => {
-        event.preventDefault()
+    const removeLink = async (linkId) => {
         const newToken = await refreshAccessToken();
         localStorage.setItem("token", newToken);
         try {
@@ -283,7 +282,7 @@ const User = () => {
                 ): (
                     <button className="button1 botonejemplo modificar" onClick={() => handleModificar(index, link.longLink)} >Modificar</button>                    
                 )}
-                <button className="button1 botonejemplo eliminar" onClick={(event) => removeLink(event, link.id)}>Eliminar</button>
+                <button className="button1 botonejemplo eliminar" onClick={() => removeLink(link.id)}>Eliminar</button>
             </div>
             {modificarIndex === index && errorsModificar.length > 0 && (
                 <div className="errores">
